2a7ef069b79bdf7c798f40efe3bcc2dd4c2d3104
Manoj <67874453+knowledgeira@users.noreply.github.com>
2022-04-17 22:49:50 +0530

    Update mj_Autoinstall.sh

--- mj_Autoinstall.sh
+++ mj_Autoinstall.sh
@@ -245,3 +245,28 @@ ansible --version | head -n 1
 
 echo -e "\n\n Remember perfection is just an illusion"
 
+
+
+:'
+
+
+On your server
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
+sudo apt install git
+
+service jenkins status
+
+
+ssh-keygen -t rsa -b 4096 -C knowledgeira@gmail.com ---->Press Enter for every question
+eval 'ssh-agent -s'
+ssh-add ~/.ssh/id_rsa
+copy your public key on localmachine -->id_rsa.pub contents to github ssh 
+'
+
+
+
